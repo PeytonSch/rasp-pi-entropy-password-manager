70db241fe5c96da451b8a1c934e256c1bd3dbd28
PeytonSch <43527622+PeytonSch@users.noreply.github.com>
2020-04-25 20:43:35 -0600

    try again

--- collect.py
+++ collect.py
@@ -1,12 +1,6 @@
 #This file will handle the reading of our sensor data from our rpi
 
-
-
-
-#
-#Im not sure if I actually want this file or not
-#
-
+import time
 
 class collector:
     def __init__(self):
